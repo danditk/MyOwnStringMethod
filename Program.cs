8f565d003b36eecf16307f5dfaca5bccc18fbcf4
Darek <darekt@abmicro.pl>
2021-11-03 23:50:04 +0100

    Function showed all text

--- Program.cs
+++ Program.cs
@@ -10,15 +10,21 @@ namespace MyOwnStringMethod
     {
         static void Main(string[] args)
         {
-            string z = "";
-            object x = 1;
-            object y = "Tekstowo";
-            TextGenerator("Jestem %0, mam %5 lata i lubię %1, a moja %2, była %3 w %4", "Wiesiek", "placki", "mama", "wczorak", "klinice dermatologicznej.", 32 );
+            //string z = "";
+            //object x = 1;
+            //object y = "Tekstowo";
+            string userText = TextGenerator("Jestem %0, mam %5 lata i lubię %1, a moja %2, była %3 w %4", "Wiesiek", "placki", "mama", "wczorak", "klinice dermatologicznej.", 32 );
+            Console.WriteLine(userText);
+
+            Console.ReadKey();
         }
 
-        public static string TextGenerator(string text, params object[] args)
+        public static string TextGenerator(string text, params object[] keyWords)
         {
-            
+            for (int i = 0; i < keyWords.Length; i++)
+            {
+                text = text.Replace("%" + i, keyWords[i].ToString());
+            }
 
             return text;
         }
